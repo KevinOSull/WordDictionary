efcc7a52628e0eb685cb574cc7352a8134fdbde5
KevinOSull <kosullivan3336@gmail.com>
2026-05-18 01:26:52 +0100

    added fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
 console.log("Hello, World!");
 console.log("This is a JavaScript file.");
+
+fetch('https://api.dictionaryapi.dev/api/v2/entries/en/objects')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
