02f14afd3a920ec03d9ff077751823410e822d6e
KevinOSull <kosullivan3336@gmail.com>
2026-05-18 12:20:14 +0100

    added button and gave it id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 console.log("Hello, World!");
 console.log("This is a JavaScript file.");
 
+let submit = document.getElementById("submitBtn");
+submit.addEventListener("click",function(){
+    console.log("Button clicked!");
+})
+
 fetch('https://api.dictionaryapi.dev/api/v2/entries/en/objects')
     .then(response => response.json())
     .then(data => {
