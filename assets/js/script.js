3ab664e29daa7d145d78f037447ae8ada02c0d59
KevinOSull <kosullivan3336@gmail.com>
2026-09-03 09:55:05 +0100

    final update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,39 @@
-console.log("Hello, World!");
-console.log("This is a JavaScript file.");
+console.log("Script loaded!");
 
-let submit = document.getElementById("submitBtn");
+let inputField = document.getElementById("inputFieldId")
+let outputField = document.getElementById("outputFieldId")
+const wordMeaning = document.getElementById("outputFieldId");
+let submit = document.getElementById("btn1");
+
+
 submit.addEventListener("click",function(){
-    console.log("Button clicked!");
+    let word = inputField.value;
+    if(!checkIsInputEmpty()){
+        outputField.innerHTML = "Please enter a word.";
+    }else{
+        fetchDefinition(word);
+    }
+});
+
+let reset = document.getElementById("btn2");
+reset.addEventListener("click",function(){
+    resetButton();
 })
 
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/objects')
+function resetButton(){
+    outputField.innerHTML = "";
+    inputField.value = "";
+}
+
+function checkIsInputEmpty(){
+    return inputField.value !== "";
+}
+
+function fetchDefinition(word){
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
     .then(response => response.json())
     .then(data => {
         console.log(data);
+        wordMeaning.innerHTML = data[0].meanings[0].definitions[0].definition;
     })
+}
